911874e9b7a73c09b6bdc76e3e019776934779e9
fulya_ture <fulyature@gmail.com>
2023-06-01 15:59:02 +0300

    exercise-3

---
+++ javascript.js
@@ -0,0 +1,93 @@
+// //?1000 ile 2000 arasında 13 e bölümünden kalan 3 veren sayıları konsola yazdırma
+// let sayac = 0;
+// for (let i = 1000; i < 2000; i++) {
+//   if (i % 13 == 3) {
+//     console.log(i);
+//     sayac++;
+//   }
+// }
+// console.log(sayac);
+//  //?Kullanıcıdan alının sayıları negatif girilene kadar toplayan ve sonucu konsola yazdıran
+
+// let durum = true;
+// let sum = 0;
+// while (durum) {
+//   let sayi = +prompt("sayı giriniz");
+//   if (sayi < 0) {
+//     break;
+//   }
+//   sum += sayi;
+// }
+// console.log(sum);
+
+// //
+
+// let durum = true;
+// let sum = 0;
+// while (durum) {
+//   let sayi = +prompt("sayı giriniz");
+//   if (sayi < 0) {
+//     durum = false;
+//     continue;
+//   }
+//   sum += sayi;
+// }
+
+// console.log(sum);
+
+//-----------------------//
+// const now = new Date();
+
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+
+// const firstName = "Fulya";
+// const lastName = "Ture";
+// const age = 35;
+// const year = new Date().getFullYear();
+// console.log(typeof firstName);
+// console.log(typeof age);
+
+// console.log(4 != "4");
+// console.log(4 !== "4");
+// console.log(!(4 > 3));
+
+// const yok = jargon.includes("on") && pyton.includes("on");
+// console.log(yok);
+
+// console.log(new Date().getFullYear());
+// console.log(new Date().getDate());
+// console.log(new Date().getDay());
+// console.log(new Date().getTime());
+
+// let a = +prompt("üçgenin yüksekliğini girin");
+// let b = +prompt("üçgenin yüksekliğini girin");
+// const result = 0.5 * a * b;
+// console.log(result);
+
+// const ad = "Fulyaaaaaaaa";
+// ad.length > 7 ? console.log("adınız uzun") : console.log("adınız kısa");
+
+// let firstName = "Fulya";
+// let lastName = "türe";
+// firstName.length > lastName.length
+//   ? console.log(`Adınız: ${firstName} daha uzun`)
+//   : firstName.length == lastName.length
+//   ? console.log("adınız soyadınız ile eşittir")
+//   : console.log(`soyadınız : ${lastName} sayadınız daha uzun`);
+
+// let myAge = 250;
+// let yourAge = 25;
+// console.log(`ı am ${myAge - yourAge} older than you`);
+
+// console.log(tarih.getFullYear(2015));
+
+// const dogumyil = 1995;
+// const kasYasida = new Date().getFullYear() - dogumyil;
+
+// kasYasida >= 18
+//   ? console.log(`ehliyet alabilir`)
+//   : console.log(`ehliyet alamaz`);
